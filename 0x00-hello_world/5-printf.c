a789261046ea73bcc511622ad16fefc858d26ae1
Santiago-Gallego <santiagoxd68@hotmail.com>
2020-02-05 18:31:38 +0000

    c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Outputs fixed message to stdio
+ * Description: Prints predetermined message using printf
+ * Return: Void
+ *
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
