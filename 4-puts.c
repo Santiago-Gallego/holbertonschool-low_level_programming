3610f65acfc13485563acd80f1d4624217f0c8a8
Santiago-Gallego <santiagoxd68@hotmail.com>
2020-02-05 18:05:00 +0000

    file c

---
+++ 4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - puts line to stdout
+ * Description: uses puts to output fixed message
+ * Return: void
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
