0e56fc116ba9a5ce53d868b432550e7dcfcb1b8a
Santiago-Gallego <santiagoxd68@hotmail.com>
2020-02-05 19:16:58 +0000

    c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - writes text to stdout
- * description: uses write to stdout
- * Return: returns 1
+ *main - prints line
+ *Return: 1
  */
 int main(void)
 {
-write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+int l;
+l = sizeof("and that piece of art is useful - Dora Korpar, 2015-10-19");
+write(2,
+"and that piece of art is useful - Dora Korpar, 2015-10-19\n", l);
 return (1);
 }
