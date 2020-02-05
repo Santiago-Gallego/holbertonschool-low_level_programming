f5b63220cb187ba59a882d2e621bbaf4be0ad563
Santiago-Gallego <santiagoxd68@hotmail.com>
2020-02-05 19:29:18 +0000

    c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,6 @@ int main(void)
 {
 int l;
 l = sizeof("and that piece of art is useful - Dora Korpar, 2015-10-19");
-write(2,
-"and that piece of art is useful - Dora Korpar, 2015-10-19\n", l);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", l);
 return (1);
 }
