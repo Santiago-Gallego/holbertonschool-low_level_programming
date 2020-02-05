75865baa817adb899ffd5046b9c4a550a1f8f925
Santiago-Gallego <santiagoxd68@hotmail.com>
2020-02-05 19:24:20 +0000

    c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
+/**
+ * main - prints standard variable size to stdout
+ * description: uses sizeof to determine variable size
+ * Return: void
+ */
 int main(void)
 {
 int l;
 l = sizeof("and that piece of art is useful - Dora Korpar, 2015-10-19");
-write(2,
-"and that piece of art is useful - Dora Korpar, 2015-10-19\n", l);
+write(2,"and that piece of art is useful - Dora Korpar, 2015-10-19\n", l);
 return (1);
 }
