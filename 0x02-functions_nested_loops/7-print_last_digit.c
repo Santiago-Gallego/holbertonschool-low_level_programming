7e1f80861cf69a7f354ff3e524267a76b7211bae
Santiago-Gallego <santiagoxd68@hotmail.com>
2020-02-11 16:07:54 +0000

    c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,30 +1,12 @@
 #include "holberton.h"
 /**
- * _abs - check the code for Holberton School students.
- * @a: is variable
- * Return: Always 0.
- */
-int _abs(int a)
-{
-	if (a >= 0)
-	{
-		return (a);
-	}
-	else
-	{
-		a = -1 * a;
-		return (a);
-	}
-	return (0);
-}
-/**
  * print_last_digit - check the code for Holberton School students.
  * @a: is variable
  * Return: Always 0.
  */
 int print_last_digit(int a)
 {
-	a = _abs(a);
+	a = a *-1;
 	a %= 10;
 	_putchar(a + '0');
 	return (a);
