4c2a384663f32f5f27b2b2e5db759a62230d9fe4
Santiago-Gallego <santiagoxd68@hotmail.com>
2020-02-11 15:24:32 +0000

    c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,12 @@
 #include "holberton.h"
 /**
  * _isalpha - check the code for Holberton School students.
- * @b: variable
+ * @c: variable
  * Return: Always 0.
  */
-int _isalpha(int b)
+int _isalpha(int c)
 {
-	if ((b >= 'a' && b <= 'z') || (b >= 'A' && b <= 'Z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);
