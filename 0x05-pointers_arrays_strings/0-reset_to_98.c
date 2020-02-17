1fb2afaef57b59521fcbb2c6fc17f744651124a4
Santiago-Gallego <60366040+Santiago-Gallego@users.noreply.github.com>
2020-02-17 16:03:12 -0500

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
 
+/**
+ *reset_to_98 - takes a pointer and updates the value to 98
+ * @n: pointer that changes value;
+ * Return: nothing;
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
