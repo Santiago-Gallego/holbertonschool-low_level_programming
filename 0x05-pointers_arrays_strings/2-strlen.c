636df78452e80e170466b142274dce37a97472a1
Santiago-Gallego <60366040+Santiago-Gallego@users.noreply.github.com>
2020-02-17 16:06:04 -0500

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * _strlen - returns length of string
+ * @s: character of string
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != 0; i++)
+	{
+	}
+	return (i);
+}
