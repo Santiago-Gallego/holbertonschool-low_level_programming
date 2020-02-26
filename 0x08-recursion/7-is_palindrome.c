8400dc91dfc91c4c1f65f7c54c97e75c647e3723
Santiago-Gallego <60366040+Santiago-Gallego@users.noreply.github.com>
2020-02-26 15:22:17 -0500

    Update 7-is_palindrome.c

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -6,10 +6,10 @@
  */
 int _strlen(char *s)
 {
-	if (!*s)
-		return (0);
-	else
-		return (1 + _strlen(s + 1));
+if (!*s)
+return (0);
+else
+return (1 + _strlen(s + 1));
 }
 /**
  * palindrome_helper - Return the length of the string
@@ -20,14 +20,14 @@ int _strlen(char *s)
  */
 int palindrome_helper(char *s, int len, int i)
 {
-	if (_strlen(s) == 0)
-		return (0);
-	if (*(s + i) != *(s + len - 1))
-		return (0);
-	else if (i > (len - 1))
-		return (1);
-	else
-		return (palindrome_helper(s, len - 1, i + 1));
+if (_strlen(s) == 0)
+return (0);
+if (*(s + i) != *(s + len - 1))
+return (0);
+else if (i > (len - 1))
+return (1);
+else
+return (palindrome_helper(s, len - 1, i + 1));
 }
 /**
  * is_palindrome - Return the length of the string
@@ -36,5 +36,5 @@ int palindrome_helper(char *s, int len, int i)
  */
 int is_palindrome(char *s)
 {
-	return (palindrome_helper(s, _strlen(s), 0));
+return (palindrome_helper(s, _strlen(s), 0));
 }
