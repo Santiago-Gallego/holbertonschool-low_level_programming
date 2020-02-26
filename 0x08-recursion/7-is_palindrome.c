e00df0793c4db15f056b92d8f26d392a6e45c5a5
Santiago-Gallego <60366040+Santiago-Gallego@users.noreply.github.com>
2020-02-26 15:14:28 -0500

    Update 7-is_palindrome.c

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,3 +1,9 @@
+#include "holberton.h"
+/**
+ * _strlen_recursion - Return the length of the string
+ * @s: String
+ * Return: Strlen
+ */
 int _strlen(char *s)
 {
 	if (!*s)
@@ -5,7 +11,13 @@ int _strlen(char *s)
 	else
 		return (1 + _strlen(s + 1));
 }
-
+/**
+ * palindrome_helper - Return the length of the string
+ * @s: String
+ * @len: size
+ * @i: iterator
+ * Return: Strlen
+ */
 int palindrome_helper(char *s, int len, int i)
 {
 	if (_strlen(s) == 0)
@@ -18,18 +30,12 @@ int palindrome_helper(char *s, int len, int i)
 	else
 		return (palindrome_helper(s, len - 1, i + 1));
 }
-
+/**
+ * is_palindrome - Return the length of the string
+ * @s: String
+ * Return: Strlen
+ */
 int is_palindrome(char *s)
 {
 	return (palindrome_helper(s, _strlen(s), 0));
-}
-int main(void)
-{
-    int r;
-
-    r = is_palindrome("level");
-    printf("%d\n", r);
-    r = is_palindrome("holbertoh");
-    printf("%d\n", r);
-    return (0);
 }
