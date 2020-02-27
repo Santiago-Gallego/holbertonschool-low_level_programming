3e2ce120841d24dcfb83af06428a7bd33b7828af
Santiago-Gallego <60366040+Santiago-Gallego@users.noreply.github.com>
2020-02-26 22:04:34 -0500

    Create 100-wildcmp.c

---
+++ 100-wildcmp.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * wildcmp - compares two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: 1 and 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*' && (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)))
+		return (1);
+	if (*s2 == '*' && *(s1 + 1) != '\0' && *s2 == '\0')
+		return (0);
+	return (0);
+}
