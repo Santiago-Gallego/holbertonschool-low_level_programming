3f2c55d57472102b02fa7c218e9accaa68ac4a8e
Santiago-Gallego <60366040+Santiago-Gallego@users.noreply.github.com>
2020-02-26 22:08:59 -0500

    Update 100-wildcmp.c

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -8,6 +8,8 @@
  */
 int wildcmp(char *s1, char *s2)
 {
+	if (*s1 == '\0')
+		return (1);
 	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 	if (*s2 == '*' && (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)))
