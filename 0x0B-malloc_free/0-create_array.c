be6253acc019a9ebf9ef522821431d3905a65aa1
Santiago-Gallego <santiagoxd68@hotmail.com>
2020-03-03 18:01:13 +0000

    fix betty

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,24 +1,23 @@
 #include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * create_array - check the code for Holberton School students.
+ * @size: size
+ * @c: pointer
+ *Return: pointer.
  */
-
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
 	char *ar;
 
-	if(size == 0)
-		return(0);
+	if (size == 0)
+		return (0);
 	ar = malloc(sizeof(char) * size);
-	if(ar == NULL)
-		return(NULL);
-	for(count = 0; count < size; count++)
+	if (ar == NULL)
+		return (NULL);
+	for (count = 0; count < size; count++)
 		ar[count] = c;
 	return (ar);
 }
