c419f574b3403f1ca45772597b34cbf0b2f2c69e
Santiago-Gallego <santiagoxd68@hotmail.com>
2020-03-03 23:44:20 +0000

    free greed

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ * free_grid - prints a grid of integers
+ * @grid: the address of the two dimensional grid
+ * @height: height of the grid
+ *
+ * Return: Nothing.
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
