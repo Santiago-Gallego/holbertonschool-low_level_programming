97a40c287319b1f919e02800c1ea44df483de761
Santiago-Gallego <60366040+Santiago-Gallego@users.noreply.github.com>
2020-03-03 13:02:34 -0500

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,7 +13,7 @@ char *create_array(unsigned int size, char c)
 	char *ar;
 
 	if (size == 0)
-		return (0);
+		return (NULL);
 	ar = malloc(sizeof(char) * size);
 	if (ar == NULL)
 		return (NULL);
