c6b343ade2df74a4a40e15f968e810bdb9005385
Santiago-Gallego <santiagoxd68@hotmail.com>
2020-03-05 17:19:46 +0000

    fix

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,39 +1,26 @@
-#include "holberton"
+#include "holberton.h"
 #include <stdlib.h>
-#include <stdio.h>
 /**
- *_memset - sets first n bytes of the memory area
- * @s: array to set
- * @b: what to set it to
- * @n: first n bytes
- * Return: s
+ * _calloc - Allocate memory for an array.
+ * @nmemb: Num memory bytes of array
+ * @size: Size of char
+ * Return: Null or the allocate memory of the array.
  */
-char *_memset(char *s, char b, unsigned int n)
-{
-        unsigned int i;
-
-        for (i = 0; i < n; i++)
-        {
-                s[i] = b;
-        }
-        return (s);
-}
-/**
- *_calloc - allocate memory for an array usingmalloc
- * @nmemb: number ofelements
- * @size: size of bytes
- * Return: pointer
- */
-
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *ptr;
+	char *allocmem = NULL;
+	unsigned int x;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	ptr = malloc(nmemb * size);
-	if (ptr == NULL)
+	allocmem = malloc(sizeof(char) * nmemb);
+	if (allocmem == NULL)
+	{
 		return (NULL);
-	_memset(ptr, 0, (nmemb * size));
-	return (ptr);
+	}
+	for (x = 0; x < nmemb; x++)
+	{
+		allocmem[x] = 0;
+	}
+	return (allocmem);
 }
