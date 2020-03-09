dc52f59e54e7859d9f96f05486ab81f397260222
Santiago-Gallego <santiagoxd68@hotmail.com>
2020-03-09 17:28:22 +0000

    refactor

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,22 +1,70 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "dog.h"
+#include <stdlib.h>
+char *_strdup(char *str);
 /**
- *new_dog - creates a new dog
- *@name: name of dog
- *@age: age of dog
- *@owner:owner of dog
- *Return: new dog struct
+ * new_dog - creates a new dog
+ * @name: variable pointing to name of dog
+ * @age: age of dog
+ * @owner: variable pointing to dog owner
+ *
+ * Return: variable pointing to dog_t struct
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	struct dog *new_dog;
+	char *s1;
+	char *s2;
+	dog_t *new_dog;
 
-	new_dog = malloc(sizeof(struct dog));
+	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
-	new_dog->name = name;
-	new_dog->age = age;
-	new_dog->owner = owner;
+	s1 = _str(name);
+	if (s1 == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+	s2 = _str(owner);
+	if (s2 == NULL)
+	{
+		free(s1);
+		free(new_dog);
+		return (NULL);
+	}
+	new_dog->name = s1;
+        new_dog->owner = s2;
+        new_dog->age = age;
 	return (new_dog);
+}
+/**
+ * _strdup - returns a pointer to dup string, new allocated memory
+* @str: string to copy
+*
+* Return: pointer to new string or NULL
+*/
+char *_str(char *str)
+{
+	char *str_2;
+	unsigned int i, j;
+	unsigned int len = 0;
+
+	if (str == NULL)
+		return (NULL);
+	j = 0;
+	while (*(str + j) != '\0')
+	{
+		len++;
+		j++;
+	}
+	str_2 = malloc((len + 1) * sizeof(char));
+	if (str_2 == NULL)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		*(str_2 + i) = *(str + i);
+		i++;
+	}
+	*(str_2 + len) = '\0';
+	return (str_2);
 }
