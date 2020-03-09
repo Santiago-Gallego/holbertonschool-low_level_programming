112710dbb3726c52e5a145c99fb16bbf54ed5d1c
Santiago-Gallego <santiagoxd68@hotmail.com>
2020-03-09 17:39:37 +0000

    refactor 2

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -29,5 +29,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 	p->name = name;
 	p->age = age;
 	p->owner = owner;
-	return (p);
+
+	struct dog *new_dog;
+
+	new_dog = malloc(sizeof(struct dog));
+	if (new_dog == NULL)
+		return (NULL);
+	new_dog->name = name;
+	new_dog->age = age;
+	new_dog->owner = owner;
+	return (new_dog);
 }
