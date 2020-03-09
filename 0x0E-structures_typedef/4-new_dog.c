c7b8db0396eff9968a9532e5824b86ff70c77412
Santiago-Gallego <santiagoxd68@hotmail.com>
2020-03-09 17:45:28 +0000

    fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -9,34 +9,17 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *p;
-
-	p = malloc(sizeof(dog_t));
-	if (p == NULL)
-		return (NULL);
-	if (name == NULL)
-	{
-		free(p);
-		free(owner);
-		return (NULL);
-	}
-	if (owner == NULL)
-	{
-		free(p);
-		free(name);
-		return (NULL);
-	}
-	p->name = name;
-	p->age = age;
-	p->owner = owner;
-
 	struct dog *new_dog;
 
 	new_dog = malloc(sizeof(struct dog));
 	if (new_dog == NULL)
 		return (NULL);
+	if (name == NULL)
+		return (NULL);
 	new_dog->name = name;
 	new_dog->age = age;
+	if (owner == NULL)
+		return (NULL);
 	new_dog->owner = owner;
 	return (new_dog);
 }
