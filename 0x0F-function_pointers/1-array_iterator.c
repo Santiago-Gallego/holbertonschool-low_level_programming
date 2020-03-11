b4654aad74fcf2087b80143629fa230d36c8f783
Santiago-Gallego <santiagoxd68@hotmail.com>
2020-03-11 18:40:02 +0000

    fix: conditional NULL

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,6 +13,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	size_t i;
 
 	ptr = action;
+	if (array != NULL || action != NULL)
 	for (i = 0; i < size; i++)
 		ptr(array[i]);
 }
