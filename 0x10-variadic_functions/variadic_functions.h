1003af8d23171312c358a716468b07de2acaedd0
Santiago-Gallego <santiagoxd68@hotmail.com>
2020-03-13 16:47:04 +0000

    [Add] First version file.

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,16 @@
+#ifndef VARFUNC
+#define VARFUNC
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+typedef struct fm
+{
+	char *fm;
+	void (*p)();
+} fm_t;
+#endif
