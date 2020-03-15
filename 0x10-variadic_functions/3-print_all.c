702ca965d0f2ddbbb709413ef8f20606ab14857e
andergcp <andergcp@hotmail.com>
2020-03-15 16:07:59 +0000

    [FIX] error loop

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,37 +3,37 @@
  * print_i - prints int
  * @list: arguement of list
  */
-void print_i(va_list list, char *s)
+void print_i(va_list list)
 {
-	printf("%s%d", s,va_arg(list, int));
+	printf("%d", va_arg(list, int));
 }
 /**
  * print_c - prints char
  * @list: arguement char
  */
-void print_c(va_list list char *s)
+void print_c(va_list list)
 {
-	printf("%s%c", s,va_arg(list, int));
+	printf("%c", va_arg(list, int));
 }
 /**
  * print_s - prints string
  * @list: list to print
  */
-void print_s(va_list list, char *ss)
+void print_s(va_list list)
 {
 	char *s = va_arg(list, char *);
 
 	if (s == NULL)
 		s = "(nil)";
-	printf("%s%s", ss, s);
+	printf("%s", s);
 }
 /**
  * print_f - prints floats
  * @list: next arguement of list to print
  */
-void print_f(va_list list, cahar *s)
+void print_f(va_list list)
 {
-	printf("%s%f", s, va_arg(list, double));
+	printf("%f", va_arg(list, double));
 }
 /**
  * print_all - prints out all stuff
@@ -43,21 +43,23 @@ void print_all(const char * const format, ...)
 {
 	va_list list;
 	int i = 0, j;
-	fm_t fm[] = {{"c", print_c}, {"i", print_i}, {"f", print_f}, {"s", print_s}};
-	char *s = ""
+	fm_t fm[] = {{"c", print_c}, {"i", print_i}, {"f", print_f}, {"s", print_s},
+		     {NULL, NULL}};
+	char *sep = "";
+
 	va_start(list, format);
-	while (format[i] != '\0' && !format)
+	while (format[i] != '\0' && format != NULL)
 	{
 		j = 0;
 		while (j < 4)
 		{
 			if (format[i] == *(fm[j]).fm)
 			{
-				printf("%s",sep);
-				fm[j].p(list, s);
-			        j++;
-				s = ", ";
-			}		
+				printf("%s", sep);
+				fm[j].p(list);
+				sep = ", ";
+			}
+			j++;
 		}
 		i++;
 	}
