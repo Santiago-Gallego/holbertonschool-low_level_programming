24d84c299c8461599983fa1fc6635ba350bd03be
Santiago-Gallego <santiagoxd68@hotmail.com>
2020-03-19 18:05:26 +0000

    [Fix]: Betty trailing whitespace on line 24 and description add_node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * _strlen - check the code for Holberton School students.
+ * _strlen - size str nodos.
  * @s: pointer string
  * Return: Always 0.
  */
@@ -13,15 +13,17 @@ int _strlen(const char *s)
 	}
 	return (i);
 }
+
 /**
- * _strlen - check the code for Holberton School students.
- * @head: nodes
- * @str: string
+ * add_node - adds a new node
+ * @head: head of node
+ * @str: string to add
+ * Return: NULL or ptr
  */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *ptr = malloc(sizeof(list_t));
-	
+
 	if (ptr == NULL)
 		return (NULL);
 	ptr->str = strdup(str);
