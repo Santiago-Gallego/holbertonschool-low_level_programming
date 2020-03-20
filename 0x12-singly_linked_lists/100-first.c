328f45bac0d067b3a5d5ab9a5e276bc6b0ea3b0a
Santiago-Gallego <santiagoxd68@hotmail.com>
2020-03-20 02:38:28 +0000

    [Add]: add 2 version

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
-void print_msg(void) __attribute__ ((first));
+void print_msg(void) __attribute__ ((constructor));
+
 /**
  *print_msg - prints a msg before the main
  */
+
 void print_msg(void)
 {
 	printf("You're beat! and yet, you must allow,\n"
