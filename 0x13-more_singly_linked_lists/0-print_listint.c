afd0f10123772327d499811d5ff7a87e87bd6098
Santiago-Gallego <santiagoxd68@hotmail.com>
2020-03-24 14:40:00 +0000

    [Fix]: Betty line 14 16

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,10 +10,10 @@ size_t print_listint(const listint_t *h)
 {
 	size_t c = 0;
 
-	while(h)
+	while (h)
 	{
 		c++;
-		printf("%d\n",h->n);
+		printf("%d\n", h->n);
 		h = h->next;
 	}
 	return (c);
