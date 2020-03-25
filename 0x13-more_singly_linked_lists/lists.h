db9aa0608301e435a8b3f48e052db53dc360a421
Santiago-Gallego <santiagoxd68@hotmail.com>
2020-03-25 19:11:54 +0000

    [Add]: prototypes

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -43,5 +43,8 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 listptr_t *add_nodeptr(listptr_t **head, const listint_t *pointer);
 listint_t *find_listint_loop(listint_t *head);
-
-#endif
+size_t free_listint_safe(listint_t **h);
+listint_t *find_listint_loop(listint_t *head);
+size_t free_listint_safe(listint_t **h);
+listint_t *find_listint_loop(listint_t *head);
+#endif /* LIST */
