161488ada6f9b551075a8358d8a46e27ca07f952
Santiago-Gallego <santiagoxd68@hotmail.com>
2020-03-26 17:36:07 +0000

    [Fix]: betty style

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,31 +6,31 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-        unsigned int num = 0;
-        int pot = 0, i = 0, val = 0;
+	unsigned int num = 0;
+	int pot = 0, i = 0, val = 0;
 
-        if(b == NULL)
-                return (0);
+	if (b == NULL)
+		return (0);
 
-        while (b[i] != '\0')
-                i++;
-        i -= 1;
-        while (i >= 0)
-        {
-                val = b[i];
-                if (val == '0')
-                {
-                        pot++;
-                        i--;
-                }
-                else if (val == '1')
-                {
-                        num = num + (1 << pot);
-                        pot++;
-                        i--;
-                }
-                else
-                        return (0);
-        }
-        return (num);
+	while (b[i] != '\0')
+		i++;
+	i -= 1;
+	while (i >= 0)
+	{
+		val = b[i];
+		if (val == '0')
+		{
+			pot++;
+			i--;
+		}
+		else if (val == '1')
+		{
+			num = num + (1 << pot);
+			pot++;
+			i--;
+		}
+		else
+			return (0);
+	}
+	return (num);
 }
