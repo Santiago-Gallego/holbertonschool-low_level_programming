f9a237656b99bd2cf662bd93a2d6c52725062b51
Santiago-Gallego <60366040+Santiago-Gallego@users.noreply.github.com>
2020-03-31 17:00:29 -0500

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	if (pf == -1)
 		return (-1);
 
-		while (text_content[i])
+		while (text_content[i] != 0)
 			i++;
 		if (text_content == NULL)
 			return (-1);
