7c753e6963a8af0873c1867c804ae6bf1a8c1636
Santiago-Gallego <60366040+Santiago-Gallego@users.noreply.github.com>
2020-03-31 17:00:16 -0500

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * create_file - check the code for Holberton School students.
+ *  create_file - check the code for Holberton School students.
  * @filename: file to create
  * @text_content:string to write
  * Return: 1 itext_contentn case of exit
@@ -11,24 +11,24 @@ int create_file(const char *filename, char *text_content)
 	int i = 0;
 	int var;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
 	pf = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (pf == -1)
 		return (-1);
 
-	while (text_content[i] != 0)
+		while (text_content[i])
 			i++;
 		if (text_content == NULL)
+			return (-1);
+
+		var = write(pf, text_content, i);
+		if (var == -1)
 		{
 			close(pf);
 			return (-1);
 		}
-
-		var = write(pf, text_content, _len(text_content));
-		if (var == -1)
-			return (-1);
 
 		close(pf);
 		return (1);
