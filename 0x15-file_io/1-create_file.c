8d22e4c5594905fec9eaac5a68e62fc930ec3a4a
Santiago-Gallego <60366040+Santiago-Gallego@users.noreply.github.com>
2020-03-31 17:03:52 -0500

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 		while (text_content[i] != 0)
 			i++;
 		if (text_content == NULL)
-			return (-1);
+			return (1);
 
 		var = write(pf, text_content, i);
 		if (var == -1)
