cb232df5bafe69e77a5223f7ba3c20447b6914a1
Santiago-Gallego <santiagoxd68@hotmail.com>
2020-03-31 19:31:08 +0000

    [Fix]: line 23, 28 and 33 betty style

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * _strlen - returns length of string
+ * _len - returns length of string
  * @s: character of string
  * Return: length of string
  */
@@ -20,17 +20,17 @@ int _len(char *s)
  * Return: 1 success -1 failed
  */
 int create_file(const char *filename, char *text_content)
-{	
+{
 	int fp;
 
 	if (filename == NULL)
 		return (-1);
-	fp = open(filename, O_CREAT | O_WRONLY | O_TRUNC , 0600);
+	fp = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fp == -1)
 		return (-1);
 	if (text_content == NULL)
 		return (1);
-	if(write(fp, text_content, _len(text_content)) == -1)
+	if (write(fp, text_content, _len(text_content)) == -1)
 	{
 		close(fp);
 		return (-1);
