f4dc9ffca6398ae8d5d5be1723c06dde198a50e0
Santiago-Gallego <60366040+Santiago-Gallego@users.noreply.github.com>
2020-03-31 17:01:03 -0500

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 	int i = 0;
 	int var;
 
-	if (!filename)
+	if (filename != NULL)
 		return (-1);
 
 	pf = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
