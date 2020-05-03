683743301a796455fdca4fbd2cc41f4749ae08b1
Santiago-Gallego <santiagoxd68@hotmail.com>
2020-05-03 20:29:24 +0000

    [Add]

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -8,47 +8,38 @@
  * @n: value of node
  * Return: the address or NULL if failed
  */
-dlistint_t *insert_dnodeint_at_index(dlistint_t **head, unsigned int index,
-				     int n)
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *temp1, *temp2, *new_node;
-	unsigned int count;
+	unsigned int i = 0;
+	dlistint_t *new_node = malloc(sizeof(dlistint_t)), *tmp_node = *h;
 
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
-		return (NULL);
-	new_node->n = n;
-	new_node->prev = NULL;
-	if (*head == NULL)
+	if (new_node)
 	{
-		if (index > 0)
+		new_node->n = n;
+		if (idx == 0)
 		{
-			free(new_node); return (NULL);
-		}
-		new_node->next = NULL; *head = new_node;
-		return (new_node);
-	}
-	if (index == 0)
-	{
-		new_node->next = *head; (*head)->prev = new_node; *head = new_node;
-		return (new_node);
-	}
-	for (count = 0, temp1 = *head; temp1 != NULL && count <= index;)
-	{
-		temp2 = temp1->next;
-		if (count == index - 1)
-		{
-			temp1->next = new_node;
-			new_node->prev = temp1;
-			if (temp2 == NULL)
-				new_node->next = NULL;
-			else
+			if (*h)
 			{
-				new_node->next = temp2; temp2->prev = new_node;
+				new_node->next = *h;
+				(*h)->prev = new_node;
 			}
+			*h = new_node;
 			return (new_node);
 		}
-		temp1 = temp1->next; count++;
+		while (i < idx - 1 && tmp_node)
+		{
+			tmp_node = tmp_node->next;
+			i++;
+		}
+		if (tmp_node)
+		{
+			if (tmp_node->next)
+				tmp_node->next->prev = new_node;
+			new_node->next = tmp_node->next;
+			new_node->prev = tmp_node;
+			tmp_node->next = new_node;
+			return (new_node);
+		}
 	}
 	return (NULL);
 }
